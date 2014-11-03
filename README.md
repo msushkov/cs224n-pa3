016c1dda02d45750c5a902d408a9eaa184a0ac81
Mikhail Sushkov <sushkovm@gmail.com>
2014-11-02 17:46:00 -0800

    better baseline

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Run from cs224n-pa3/java directory:
 java -Xmx500m -cp "extlib/*:classes" cs224n.assignments.CoreferenceTester -path ../data -model baseline -data dev -documents 100
+
+java -Xmx500m -cp "extlib/*:classes" cs224n.assignments.CoreferenceTester -path ../data -model BetterBaseline -data dev -documents 100
--- java/src/cs224n/corefsystems/BetterBaseline.java
+++ java/src/cs224n/corefsystems/BetterBaseline.java
@@ -1,39 +1,77 @@
 package cs224n.corefsystems;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 
-
-import cs224n.coref.ClusteredMention;
-import cs224n.coref.Document;
 import cs224n.coref.*;
+import cs224n.util.CounterMap;
 import cs224n.util.Pair;
 
 public class BetterBaseline implements CoreferenceSystem {
 
-	@Override
-	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		// TODO Auto-generated method stub
+    CounterMap<String, String> counts = new CounterMap<String, String>();
+    int MIN_COUNT = 5;
 
-	}
+    @Override
+    public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
+        // count pairwise corefs
+        for (Pair<Document, List<Entity>> point : trainingData) {
+            Document doc = point.getFirst();
+            for (Entity e : point.getSecond()) {
+                for (Pair<Mention, Mention> pair : e.orderedMentionPairs()) {
+                    Mention m1 = pair.getFirst();
+                    Mention m2 = pair.getSecond();
+                    Sentence sent1 = m1.sentence;
+                    Sentence sent2 = m2.sentence;
+                    String m1Text = sent1.lemmas.get(m1.headWordIndex);
+                    String m2Text = sent2.lemmas.get(m2.headWordIndex);
+                    counts.incrementCount(m1Text, m2Text, 1);
+                    counts.incrementCount(m2Text, m1Text, 1);
+                }
+            }
+        }
+    }
 
-	@Override
-	public List<ClusteredMention> runCoreference(Document doc) {
-		// TODO Auto-generated method stub
-	
-	ArrayList<ClusteredMention> clusters = new ArrayList<ClusteredMention>();
-	for (Mention m : doc.getMentions()) {
-        if (m.gloss().equals("God the Protector")) {
-        System.out.println(m.sentence.parse);
-	System.out.println(m.parse);
-        System.out.println(m.beginIndexInclusive + " "+m.endIndexExclusive);
-        System.out.println(m.gloss());
-}
-     clusters.add(m.markSingleton());
-}
+    @Override
+    public List<ClusteredMention> runCoreference(Document doc) {
+        ArrayList<ClusteredMention> clusters = new ArrayList<ClusteredMention>();
+        ArrayList<Entity> entities = new ArrayList<Entity>();
 
-	return clusters;
-	}
+        // if this mention is not coreferent with any of our existing clusters, make it its own cluster
+        for (Mention m : doc.getMentions()) {
+            // go through all the entities; if find a match, then add it to that cluster; otherwise, make it its own
+            boolean isMatch = false;
+            for (Entity e : entities) {
+                if (checkClusterCoref(m, e)) {
+                    clusters.add(m.markCoreferent(e));
+                    isMatch = true;
+                    break;
+                }
+            }
+            if (!isMatch) {
+                ClusteredMention newCluster = m.markSingleton();
+                clusters.add(newCluster);
+                entities.add(newCluster.entity);
+            }
+        }
+        return clusters;
+    }
 
+    // checks if given mention is coreferent with all members of the cluster
+    public boolean checkClusterCoref(Mention testMention, Entity e) {
+        String testHeadLemma = testMention.sentence.lemmas.get(testMention.headWordIndex);
+
+        for (Mention m : e.mentions) {
+            String currHeadLemma = m.sentence.lemmas.get(m.headWordIndex);
+            Double c1 = counts.getCount(testHeadLemma, currHeadLemma);
+            Double c2 = counts.getCount(currHeadLemma, testHeadLemma);
+
+            if (!(currHeadLemma.equals(testHeadLemma) || c1 > MIN_COUNT || c2 > MIN_COUNT)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
